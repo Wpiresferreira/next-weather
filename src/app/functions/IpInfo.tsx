5e3f91a065f14da07febd8108f61e34abac909b5
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-08-26 17:38:07 -0600

    test for geolocation VERCEL

--- src/app/functions/IpInfo.tsx
+++ src/app/functions/IpInfo.tsx
@@ -1,14 +1,14 @@
-export default async function IpInfo(props: { ip: string | null }) {
-  let resObj = await fetch(
-    "https://ipgeolocation.abstractapi.com/v1/?api_key=c1f85b055e204d9b89283c0c81b4053c&ip_address=" +
-      props.ip
-  ).then((res) => res.json());
+export default async function IpInfo(props: { ip: string | null, latitude: string | null, longitude : string | null, country: string | null }) {
+//   let resObj = await fetch(
+//     "https://ipgeolocation.abstractapi.com/v1/?api_key=c1f85b055e204d9b89283c0c81b4053c&ip_address=" +
+//       props.ip
+//   ).then((res) => res.json());
 
   const url =
     "https://weatherapi-com.p.rapidapi.com/current.json?q=" +
-    resObj.latitude +
+    props.latitude +
     "%2C" +
-    resObj.longitude;
+    props.longitude;
   const options = {
     method: "GET",
     headers: {
@@ -27,16 +27,30 @@ export default async function IpInfo(props: { ip: string | null }) {
     bg_colors = "from-blue-800 to-black text-white";
   }
 
+  let weatherYYC = await fetch( "https://weatherapi-com.p.rapidapi.com/current.json?q=" +
+    '51.042387'
+ +
+    "%2C" + '-114.084537', options)
+    .then((res) => res.json());
+
+  let isDayYYC = weatherYYC.current.is_day
+  let bg_colorsYYC;
+  if (isDayYYC == 1) {
+    bg_colorsYYC = "from-blue-300 to-sky-100 text-black";
+  } else {
+    bg_colorsYYC = "from-blue-800 to-black text-white";
+  }
+
   return (
-    <>
+    <div className="flex flex-col">
     <div
-      className={` ${bg_colors} flex flex-col self-center p-6 max-w-[300px] items-center justify-center bg-gradient-to-br rounded-3xl mx-4 `}
+      className={` ${bg_colors} m-2 flex flex-col self-center p-6 max-w-[300px] items-center justify-center bg-gradient-to-br rounded-3xl mx-4 `}
       >
       <div className="flex items-center">
         <h1>{weather.location.name}</h1>
         <img
           className="px-3"
-          src={resObj.flag.png ?resObj.flag.png : "https://placehold.co/600x400?text=Hello+World"}
+          src={"https://static.abstractapi.com/country-flags/" + props.country + "_flag.svg"}
           width={70}
           />
       </div>
@@ -55,6 +69,34 @@ export default async function IpInfo(props: { ip: string | null }) {
       </div>
       <h1>{weather.current.condition.text}</h1>
     </div>
-            </>
+
+
+    <div
+      className={` ${bg_colorsYYC} flex flex-col  m-2 self-center p-6 max-w-[300px] items-center justify-center bg-gradient-to-br rounded-3xl mx-4 `}
+      >
+      <div className="flex items-center">
+        <h1>{weatherYYC.location.name}</h1>
+        <img
+          className="px-3"
+          src={'https://static.abstractapi.com/country-flags/CA_flag.svg'}
+          width={70}
+          />
+      </div>
+      <div className="flex items-center">
+        <img
+          src={
+              weatherYYC.current.condition.icon
+              ? weatherYYC.current.condition.icon
+              : "https://placehold.co/600x400?text=Hello+World"
+            }
+            width={100}
+            />
+        <h1 className="antialiased font-bold text-3xl ">
+          {weatherYYC.current.temp_c}ºC
+        </h1>
+      </div>
+      <h1>{weatherYYC.current.condition.text}</h1>
+    </div>
+            </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import IpInfo from "./functions/IpInfo";
 
 export default async function Home() {
   const ip = await headers().get("x-forwarded-for");
+  const latitude = await headers().get("x-vercel-ip-latitude");
+  const longitude = await headers().get("x-vercel-ip-longitude");
+  const country =  await headers().get("x-vercel-ip-country");
 
   return (
     <main className="text-2xl flex items-center justify-center">
-      <IpInfo ip={ip} />
+      <IpInfo ip={ip} latitude={latitude} longitude={longitude} country={country} />
     </main>
   );
 }
